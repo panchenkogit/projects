dd5f3ea238480d9be9957de2a82dfd659c7cfa82
unknown <cs_go2010@mail.ru>
2024-09-12 23:30:00 +0500

    реализованы состояния FMS,теперь никаких глобальных перемен.Фильтр для сообщений.Исправь запись в бд завтра умоляюgit status

--- TelegramBots/TGBotFromZero/handlers.py
+++ TelegramBots/TGBotFromZero/handlers.py
@@ -1,6 +1,8 @@
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram import F, Router
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 
 import asyncio
 import random
@@ -12,26 +14,43 @@ import dictionary as dict
 
 
 router = Router()
-word = ''
+
+class Word(StatesGroup):
+    give_word = State()
+    get_translate = State()
+
+@router.message(~F.text & ~F.text.startswith('/'))
+async def handle_non_text(message: Message):
+    await message.answer("Это сообщение не является текстом или командой. Что вы хотели отправить?")
 
 @router.message(CommandStart())
-async def cmd_start(message: Message): 
-    user_bot = await rq.add_user(message.from_user.id, message.from_user.username)
-    if not user_bot:
-        if message.from_user.id == ADMIN_ID:
-            await message.answer(f"Добро пожаловать, администратор {message.from_user.username}", reply_markup=kb.admin_keyboard)
-        else:
-            await message.answer("Привет!Я бот для обучения английскому языку.Нажми на кнопку,когда будешь готов.\nПодробнее о формате обучения можете узнать,написав команду /help", reply_markup=kb.main)
-    else:     
-        await message.answer(f"Привет, {message.from_user.username}", reply_markup=kb.main)
+async def cmd_start(message: Message):
+    user_id = message.from_user.id
+    username = message.from_user.username
+    if username is None:
+        username = "user" 
+
+    try:
+        user_bot = await rq.add_user(user_id, username)
+    except Exception as e:
+        await message.answer("Произошла ошибка при добавлении пользователя. Попробуйте еще раз.")
+        return
+    
+    if message.from_user.id == ADMIN_ID:
+        await message.answer(f"Добро пожаловать, администратор {username}", reply_markup=kb.admin_keyboard)
+    elif not user_bot:
+        await message.answer(f"Привет, {username}", reply_markup=kb.main)
+    else:
+        await message.answer("Привет! Я бот для обучения английскому языку. Нажми на кнопку, когда будешь готов.\nПодробнее о формате обучения можете узнать, написав команду /help", reply_markup=kb.main)
+
 @router.message(Command('help'))
 async def get_help(message: Message):
     await message.answer("Прицпин простой.Вам присылают слово на английском языке.Ваша задача-перевести его и написать в чат ответ.Если он верный,то вам будет отправлено следующее слово.Если нет,у вас будет бесконечное число попыток дял ввода верного варианта ответа.")
 
 @router.callback_query(F.data == 'start_learning')
-async def start_learning(callback: CallbackQuery):
+async def start_learning(callback: CallbackQuery, state: FSMContext):
     await callback.answer("Вы начали обучение")
-    await get_word(callback.message)
+    await get_word(callback.message, state)
     
 @router.callback_query(F.data == 'set')
 async def set(callback: CallbackQuery):
@@ -62,24 +81,22 @@ async def user_action(callback: CallbackQuery):
     await callback.answer(f"Вы выбрали пользователя {user.username}")  
     await callback.message.answer(f"Действия с пользователем {user.username}", reply_markup=await kb.user_action_keyboard(tg_id))
 
-
 @router.message(Command('word'))
-async def get_word(message: Message):
-    global word
-    word = random.choice(list(dict.word_dict.keys()))
-    await message.answer(f"Переведите слово {word}")
+async def get_word(message: Message, state: FSMContext):
+        word = random.choice(list(dict.word_dict.keys()))
+        await state.update_data(give_word=word)
+        await message.answer(f"Переведите слово {word}")
+        await state.set_state(Word.get_translate)
 
-@router.message()
-async def check_translate(message: Message):
-    answer = message.text.strip().lower()
-    right = dict.word_dict[word]
-    if answer == right:
-        await message.answer(f"Верно! {word} переводится как {right}")
+@router.message(Word.get_translate)
+async def check_translate(message: Message, state: FSMContext):
+    await state.update_data(get_translate=message.text)
+    data = await state.get_data()
+    right = dict.word_dict[data['give_word']].lower()
+    if data['get_translate'].lower() == right:
+        await message.answer(f"Верно! {data['give_word']} переводится как {right}")
         await asyncio.sleep(5)
-        await get_word(message)
+        await get_word(message, state)
     else:
          await message.answer("Неверно! Попробуй ещё раз или введи /word, чтобы пропустить это слово.")
 
-    
-
-        
